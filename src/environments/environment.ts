0e5889b99c65b420cf1c7cedbdba305219d0c056
Richard Olson <richard@richardolson.org>
2025-03-27 21:05:40 -0500

    Updated socket endpoint.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ import { AppConfig } from "../model/app-config.models";
 export const environment: AppConfig = {
     production: true,
     apiBaseUrl: 'https://www.richardolson.org/api/',
-    chatSocketIoEndpoint: 'ws://richardolson.org/chat-io'
+    chatSocketIoEndpoint: 'ws://richardolson.org/api/chat-io'
 };

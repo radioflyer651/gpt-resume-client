65b1a4ac8ccb1221c8510e5926869a02cfcd75a5
Richard Olson <richard@richardolson.org>
2025-03-28 00:17:20 -0500

    Update chatSocketIoEndpoint to remove API path for chat service

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,6 @@ import { AppConfig } from "../model/app-config.models";
 export const environment: AppConfig = {
     production: true,
     apiBaseUrl: 'https://www.richardolson.org/api/',
-    chatSocketIoEndpoint: 'https://www.richardolson.org/api/chat-io',
+    chatSocketIoEndpoint: 'https://www.richardolson.org',
     chatSocketPath: '/chat-io/'
 };
